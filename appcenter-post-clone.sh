22293b44f47229ec4ee186326f9d68c90bf52a59
tharun05 <tharundillikar@gmail.com>
2024-08-16 02:42:47 +0530

    commit

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-# Ensure Java 11 is used
-echo "Configuring Java 11 for the build..."
+echo "Checking Java version..."
+java -version
 
+# If necessary, set Java 11 as the default
 sudo update-alternatives --set java /usr/lib/jvm/java-11-openjdk-amd64/bin/java
 sudo update-alternatives --set javac /usr/lib/jvm/java-11-openjdk-amd64/bin/javac
 
+echo "Java version after setting:"
 java -version
